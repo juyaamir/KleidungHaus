81e583f2c309b6ad76151ed5343c5bc69e081ba9
Juya <juyaamirm@gmail.com>
2024-11-15 13:38:06 +0100

    react icons were added to navbar

--- src/app/components/desk-navbar.tsx
+++ src/app/components/desk-navbar.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
+
+import { IoBagHandleOutline } from "react-icons/io5";
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+
 const DeskNavbar = () => {
   return (
     <div className=' bg-[#222121] text-white px-4'>
-      <nav className='flex gap-4 md:text-xl max-w-[1200px] mx-auto'>
-        <Link href='/' className='p-2'>
+      <nav className='flex md:text-xl max-w-[1200px] mx-auto'>
+        <Link href='/' className='px-4 py-2 border-r-[#858282] border-r-[1px]'>
           KleidundHaus
         </Link>
-        <Link href='/damen' className='p-2 '>Damen</Link>
-        <Link href='/herren' className='p-2'>Herren</Link>
-        <Link href='/kinder' className='p-2'>Kinder</Link>
-        <div className='grow p-2' >Search...</div>
-        <Link href='/saved-list' className='p-2'>Favorite</Link>
-        <Link href='/cart' className='p-2'>Cart</Link>
-        <div className='p-2 hover:cursor-pointer'>User</div>
+        <Link href='/damen' className='px-4 py-2 border-r-[#858282] border-r-[1px] '>Damen</Link>
+        <Link href='/herren' className='px-4 py-2 border-r-[#858282] border-r-[1px]'>Herren</Link>
+        <Link href='/kinder' className='px-4 py-2 border-r-[#858282] border-r-[1px]'>Kinder</Link>
+        <div className='grow px-4 py-2' >Search...</div>
+        <Link href='/saved-list' className='px-4 py-2 flex justify-center items-center'>
+        <FaRegHeart size='26px' /></Link>
+        <Link href='/cart' className='px-4 py-2 flex justify-center items-center'>
+        <IoBagHandleOutline size='26px' height='20px' /></Link>
+        <div className='px-4 py-2 hover:cursor-pointer flex justify-center items-center'>
+          <FaRegUser size='26px'/></div>
       </nav>
     </div>
   )
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,7 @@ import DeskNavbar from "./desk-navbar"
 import Banner from "./banner"
 const Header = () => {
   return (
-    <header className="border w-full bg-[#302e2e]">
+    <header className="border w-full bg-[#635d5d]">
       <Banner />
       <DeskNavbar />
     </header>
